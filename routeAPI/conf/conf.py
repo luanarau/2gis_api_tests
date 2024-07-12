17f9973531736e2bec9206b99a0be5860c8f39be
luanarau <luanarautha@gmail.com>
2024-07-12 23:56:08 +0700

    routeAPI tests

--- routeAPI/conf/conf.py
+++ routeAPI/conf/conf.py
@@ -1,11 +1,25 @@
+import pytest
+
 BASE_URL = 'http://routing.api.2gis.com/routing/7.0.0/global'
 
 KEY = 'e7f5231f-6951-449a-8ba4-bbefe834b54a'
 
 STATUSCODES = [200, 400, 403, 404, 408, 500]
 
-def make_header(body: dict) -> str:
-    return 'Content-Type: application/json' + f'--data {body}'
+easy_route = {
+
+    "points": [
+        {"lon": 82.8967, "lat": 54.9832, "type": "stop"},
+        {"lon": 82.9018, "lat": 54.9862, "type": "stop"}
+    ],
+    "transport": "car",
+    "route_mode": "fastest",
+    "traffic_mode": "jam",
+    "output": "summary"
+
+}
+
+
 
 
 
--- routeAPI/exersise_api_test.py
+++ routeAPI/exersise_api_test.py
@@ -1,22 +1,40 @@
 from routeAPI.classMethods.Post import POST_query_Responce
+from routeAPI.conf.conf import easy_route
 
-
-dict_test = {
-
-    "points": [
-        {"lon": 82.8967, "lat": 54.9832, "type": "stop"},
-        {"lon": 82.9018, "lat": 54.9862, "type": "stop"}
-    ],
-    "transport": "car",
-    "route_mode": "fastest",
-    "traffic_mode": "jam",
-    "output": "summary"
-
-}
 
 def test_1():
-    post = POST_query_Responce(dict_test).validate_answer_query()
-    a = [i for i in range(10)]
-    b = [i for i in a if i < 5]
-    print(b)
+    post = POST_query_Responce(easy_route).validate_answer_query().validate_result_status('OK')
 
+
+def test_2():
+    route = easy_route
+    post = POST_query_Responce(route).validate_answer_query().validate_result_status('OK')
+    length = post.answer_body[0]['length']
+    for i in range(10):
+        easy_route["points"][0]["lon"] = round(easy_route["points"][0]["lon"] - 0.01, 4)
+        easy_route["points"][0]["lat"] = round(easy_route["points"][0]["lat"] - 0.01, 4)
+
+        easy_route["points"][1]["lon"] = round(easy_route["points"][1]["lon"] + 0.01, 4)
+        easy_route["points"][1]["lat"] = round(easy_route["points"][1]["lat"] + 0.01, 4)
+
+        next_post = POST_query_Responce(easy_route).validate_answer_query().validate_result_status('OK')
+        next_length = next_post.answer_body[0]['length']
+        assert length < next_length, 'Wrong calculated responce lenght :('
+        length = next_length
+
+
+def test_3():
+    route = easy_route
+    post = POST_query_Responce(route).validate_answer_query().validate_result_status('OK')
+    length = post.answer_body[0]['length']
+    for i in range(10):
+        easy_route["points"][0]["lon"] = round(easy_route["points"][0]["lon"] + 0.0001, 4)
+        easy_route["points"][0]["lat"] = round(easy_route["points"][0]["lat"] + 0.0001, 4)
+
+        easy_route["points"][1]["lon"] = round(easy_route["points"][1]["lon"] - 0.0001, 4)
+        easy_route["points"][1]["lat"] = round(easy_route["points"][1]["lat"] - 0.0001, 4)
+
+        next_post = POST_query_Responce(easy_route).validate_answer_query().validate_result_status('OK')
+        next_length = next_post.answer_body[0]['length']
+        assert length > next_length, 'Wrong calculated responce lenght :('
+        length = next_length
